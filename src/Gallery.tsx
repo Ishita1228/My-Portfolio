564d775fe428140a15fe872d6f3b365447367820
Ishita1228 <24131407@studentmail.ul.ie>
2024-12-06 02:20:24 +0000

    www

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import styles from './Gallery.module.css'; // CSS Module for styling
+import coffeevideo from '../public/coffeevideo.mp4';
+import photo1 from '../public/middlesectionphoto.jpeg';
+import photo2 from '../public/aestheticphoto.jfif';
+import photo3 from '../public/passionphoto.jpeg';
 
 const Gallery = () => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Gallery</h1>
+      <div className={styles.grid}>
+        <div className={styles.gridItem}>
+          <img src={photo1} alt="Middle Section" className={styles.image} />
+        </div>
+        <div className={styles.gridItem}>
+          <img src={photo2} alt="Aesthetic" className={styles.image} />
+        </div>
+        <div className={styles.gridItem}>
+          <img src={photo3} alt="Passion" className={styles.image} />
+        </div>
+        <div className={styles.gridItem}>
+          <video src={coffeevideo} controls className={styles.video}></video>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Gallery
+export default Gallery;
